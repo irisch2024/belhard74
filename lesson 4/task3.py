f2a5a44d96ff18b11d8bc6b7918442e769d21ea1
irisch2024 <irisch@tut.by>
2024-02-05 17:57:55 +0300

    add .idea to ignore

--- lesson 4/task3.py
+++ lesson 4/task3.py
@@ -3,9 +3,8 @@ a = input("Имя: ")
 bib = {"name": a}
 b = input("Почта: ")
 bibl = {"email": b}
-N = int(input("Введите размер списка: "))
-#key = list(range(1, N+1))
-biblio = {N: bib}
+N = int(input("Введите число: "))
+biblio = {N: [bib, bibl]}
 print(biblio)
 
 

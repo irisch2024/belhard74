9ea0b53d58a2e78502ac70ab7dfaa1dd2f9f87a1
irisch2024 <irisch@tut.by>
2024-02-05 17:55:00 +0300

    add .idea to ignore

---
+++ lesson 4/task3.py
@@ -0,0 +1,12 @@
+
+a = input("Имя: ")
+bib = {"name": a}
+b = input("Почта: ")
+bibl = {"email": b}
+N = int(input("Введите размер списка: "))
+#key = list(range(1, N+1))
+biblio = {N: bib}
+print(biblio)
+
+
+

95210173822030cadb28933352cb9092ab36941f
irisch2024 <irisch@tut.by>
2024-02-07 15:08:25 +0300

    add .idea to ignore

--- lesson 5/task2.py
+++ lesson 5/task2.py
@@ -1,8 +1,8 @@
 # Сделать калькулятор: у пользователя спрашивается число, потом действие и второе число
 
-a = float(input())
-с = str(input())
-b = float(input())
+a = float(input("Введите число: "))
+с = str(input("Введите действие: "))
+b = float(input("Введите число: "))
 
 if с == "+":
     m = a + b
@@ -12,6 +12,16 @@ elif с == "-":
     m = a - b
     print(m)
 
+elif с == "/":
+    m = a / b
+    print(m)
+
+elif с == "*":
+    m = a * b
+    print(m)
+
+else:
+    print("Неверные данные")
 
 
 

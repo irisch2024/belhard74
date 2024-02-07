82f3dc725d9da2511c36083e7778ab46c877e740
irisch2024 <irisch@tut.by>
2024-02-07 12:27:11 +0300

    add .idea to ignore

--- lesson 5/task1.py
+++ lesson 5/task1.py
@@ -1,11 +1,28 @@
 # Вывести первые N чисел кратные M и больше K
 N = 5
 M = 3
-K = -5
+P = -6
 
-for i in range(K, 1000):
-    if i % M == 0 and i > K:
-        print(i)
+
+i = k = P
+while i != N:
+    if k % M == 0 and k > P:
+        print(k)
+        i += 1
+    k += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

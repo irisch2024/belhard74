bb2655f02a7ddd817447a87e41bd4584bba8aafe
irisch2024 <irisch@tut.by>
2024-02-08 14:10:39 +0300

    add .idea to ignore

--- lesson 5/task1.py
+++ lesson 5/task1.py
@@ -1,15 +1,14 @@
 # Вывести первые N чисел кратные M и больше K
 N = 5
-M = 5
-P = -6
+M = 3
+K = -10
 
+i = K
+while i < N:
+    if i % M == 0:
+        print(i)
+    i += 1
 
-i = k = P
-while i != N:
-    if k % M == 0 and k > P:
-        print(k)
-        i += 1
-    k += 1
 
 
 

26349ac3bb013da2b7275e433a0295a260d63bfa
irisch2024 <irisch@tut.by>
2024-02-08 11:45:23 +0300

    add .idea to ignore

--- lesson 5/task3.py
+++ lesson 5/task3.py
@@ -4,8 +4,14 @@ N = int(input())
 
 for i in range(2,N+1):
     if i % 2 == 0:
-        print(i, end=' ')
-    elif i % 10 == 0:
+        print(i, end=" ")
+
+        if i % 10 == 0:
+            print(end=" \n")
+
+
+
+
 
 
 

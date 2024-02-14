862f7893f7dedf61db8f820ff84790931bc63391
irisch2024 <irisch@tut.by>
2024-02-14 18:00:55 +0300

    add .idea to ignore

---
+++ lesson 7/task1.py
@@ -0,0 +1,16 @@
+#Написать класс Car
+
+class Car:
+    cls_color = "Green"   #цвет
+    obj_color: str
+
+    cls_count_passenger_seats = 3 #количество пассажирских мест
+    obj_count_passenger_seats = int
+
+    cls_is_baby_seat = "в наличии" #наличие десткого кресла
+    obj_is_baby_seat = bool
+
+
+
+
+

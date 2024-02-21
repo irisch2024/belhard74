8018e970ba9f91f47465871802ae3cf58caa40bf
irisch2024 <irisch@tut.by>
2024-02-21 12:07:17 +0300

    add .idea to ignore

--- lesson 7/task1.py
+++ lesson 7/task1.py
@@ -8,9 +8,14 @@ class Car:
         self.s_baby_seat = s_baby_seat
         self.is_busy = False
 
+
+    def __str__(self):
+        return f"color={self.color} count_passenger_seats={self.count_passenger_seats} s_baby_seat={self.s_baby_seat} is_busy={self.is_busy}"
+
+
 Car1 = Car(color="Green", count_passenger_seats=4, s_baby_seat = True)
 Car2 = Car(color="Red", count_passenger_seats=4, s_baby_seat = False)
 
-
 print(Car2.s_baby_seat)
-
+print(Car1)
+print(Car2)

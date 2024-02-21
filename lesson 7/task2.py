251d50be89357e5e07425dd860336d6a22443ead
irisch2024 <irisch@tut.by>
2024-02-21 16:21:09 +0300

    add .idea to ignore

---
+++ lesson 7/task2.py
@@ -0,0 +1,10 @@
+class Car:
+
+    def __init__(self, color, count_passenger_seats, s_baby_seat):
+        self.color = color
+        self.count_passenger_seats = count_passenger_seats
+        self.s_baby_seat = s_baby_seat
+        self.is_busy = False
+
+
+class Taxi:
